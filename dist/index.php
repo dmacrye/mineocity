2096465c1a1736e4a53f8e94643634091e98749f
Connor Peet <connor@connorpeet.com>
2014-03-04 17:28:16 -0600

    Format

--- dist/index.php
+++ dist/index.php
@@ -6,68 +6,81 @@ require 'vendor/autoload.php';
 $app = new \Slim\Slim;
 
 // Register a singleton of the Mustache engine, and tell it to cache
-$app->container->singleton('mustache', function () {
-	return new Mustache_Engine(array(
-		'cache' => 'storage',
-		'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/pages', array('extension' => '.html'))
-	));
-});
+$app->container->singleton(
+    'mustache',
+    function () {
+        return new Mustache_Engine(array(
+            'cache' => 'storage',
+            'loader' => new Mustache_Loader_FilesystemLoader(dirname(
+                        __FILE__
+                    ) . '/pages', array('extension' => '.html'))
+        ));
+    }
+);
 
 // Helper function to render templates
-function renderTemplate($name, $data = array()) {
-	global $app;
+function renderTemplate($name, $data = array())
+{
+    global $app;
 
-	if (file_exists('config.php')) {
-		$data = (require 'config.php');
-	}
+    if (file_exists('config.php')) {
+        $data = (require 'config.php');
+    }
 
-	$data += array(
-		'resourceUri' => $app->request->getResourceUri() ?: 'index',
-		'request' => $app->request
-	);
+    $data += array(
+        'resourceUri' => $app->request->getResourceUri() ? : 'index',
+        'request' => $app->request
+    );
 
-	return $app->mustache->loadTemplate($name)->render($data);
+    return $app->mustache->loadTemplate($name)->render($data);
 }
 
 // Loads a page by the given name/path
-function loadPage($path) {
-	global $app;
+function loadPage($path)
+{
+    global $app;
 
-	// Set up the base path
-	$f = 'pages/' . $path;
-	
-	if (file_exists($f . '.html')) {
-		// If there's an HTML file, render the mustache template
-		return renderTemplate($path . '.html');
-	} elseif (file_exists($f . '.php')) {
-		// If there's a PHP file, return it
-		return (require $f . '.php');
-	} elseif ($path != '404') {
-		// Otherwise, go get the 404 page
-		return loadPage('404');
-	} else {
-		// But if the user doesn't have a 404 page made, return a plain 404
-		$app->halt(404);
-	}
+    // Set up the base path
+    $f = 'pages/' . $path;
+
+    if (file_exists($f . '.html')) {
+        // If there's an HTML file, render the mustache template
+        return renderTemplate($path . '.html');
+    } elseif (file_exists($f . '.php')) {
+        // If there's a PHP file, return it
+        return (require $f . '.php');
+    } elseif ($path != '404') {
+        // Otherwise, go get the 404 page
+        return loadPage('404');
+    } else {
+        // But if the user doesn't have a 404 page made, return a plain 404
+        $app->halt(404);
+    }
 }
 
 // Index page
-$app->get('/', function () use ($app) {
-	echo loadPage('index');
-});
+$app->get(
+    '/',
+    function () use ($app) {
+        echo loadPage('index');
+    }
+);
 
 
 // Route to everything else
-$app->get('/.*?', function () use ($app) {
-	// Get the current request path
-	$path = $app->request->getResourceUri();
+$app->get(
+    '/.*?',
+    function () use ($app) {
+        // Get the current request path
+        $path = $app->request->getResourceUri();
 
-	// Make sure the user isn't trying to escape and do nasty things
-	if (!preg_match('/^[A-z\/\-\_]+$/', $path)) {
-		echo loadPage('404');
-	}
+        // Make sure the user isn't trying to escape and do nasty things
+        if (!preg_match('/^[A-z\/\-\_]+$/', $path)) {
+            echo loadPage('404');
+        }
 
-	// Send the page off to get loaded
-	echo loadPage($path);
-});
+        // Send the page off to get loaded
+        echo loadPage($path);
+    }
+);
 $app->run();
