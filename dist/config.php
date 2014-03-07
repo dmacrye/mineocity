aa7f2be1e51f83fbf60b26c57d67ced16c6d264b
Connor Peet <connor@connorpeet.com>
2014-03-07 14:48:23 -0600

    Mc plans page sans images

--- dist/config.php
+++ dist/config.php
@@ -5,5 +5,19 @@ return array(
         'locations' => array(
             array('x' => '10px', 'y' => '30px')
         )
+    ),
+    'minecraft' => array(
+        'plans' => array(
+            array('name' => 'Air', 'ram' => 512, 'slots' => 7, 'usa_link' => 9, 'europe_link' => 100),
+            array('name' => 'Dirt', 'ram' => 1024, 'slots' => 15, 'usa_link' => 1, 'europe_link' => 101),
+            array('name' => 'Grass', 'ram' => 1536, 'slots' => 23, 'usa_link' => 20, 'europe_link' => 109),
+            array('name' => 'Cobblestone', 'ram' => 2048, 'slots' => 30, 'usa_link' => 2, 'europe_link' => 102),
+            array('name' => 'Stone', 'ram' => 3072, 'slots' => 45, 'usa_link' => 3, 'europe_link' => 103),
+            array('name' => 'Coal', 'ram' => 4096, 'slots' => 60, 'usa_link' => 4, 'europe_link' => 104),
+            array('name' => 'Iron', 'ram' => 5120, 'slots' => 100, 'usa_link' => 5, 'europe_link' => 105),
+            array('name' => 'Redstone', 'ram' => 6144, 'slots' => 120, 'usa_link' => 6, 'europe_link' => 106),
+            array('name' => 'Gold', 'ram' => 7168, 'slots' => 140, 'usa_link' => 7, 'europe_link' => 107),
+            array('name' => 'Diamond', 'ram' => 8192, 'slots' => 160, 'usa_link' => 8, 'europe_link' => 108)
+        )
     )
 );
