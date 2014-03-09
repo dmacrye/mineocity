cac00a4c7b36dbf0c3c70afe9d383c25996e5401
Connor Peet <connor@connorpeet.com>
2014-03-08 23:01:12 -0600

    Prices in plans

--- dist/config.php
+++ dist/config.php
@@ -8,14 +8,14 @@ return array(
     ),
     'minecraft' => array(
         'plans' => array(
-            array('name' => 'Air', 'ram' => 512, 'slots' => 7, 'usa_link' => 9, 'europe_link' => 100),
-            array('name' => 'Dirt', 'ram' => 1024, 'slots' => 15, 'usa_link' => 1, 'europe_link' => 101),
-            array('name' => 'Grass', 'ram' => 1536, 'slots' => 23, 'usa_link' => 20, 'europe_link' => 109),
-            array('name' => 'Cobblestone', 'ram' => 2048, 'slots' => 30, 'usa_link' => 2, 'europe_link' => 102),
-            array('name' => 'Stone', 'ram' => 3072, 'slots' => 45, 'usa_link' => 3, 'europe_link' => 103),
-            array('name' => 'Coal', 'ram' => 4096, 'slots' => 60, 'usa_link' => 4, 'europe_link' => 104),
-            array('name' => 'Redstone', 'ram' => 6144, 'slots' => 120, 'usa_link' => 6, 'europe_link' => 106),
-            array('name' => 'Diamond', 'ram' => 8192, 'slots' => 160, 'usa_link' => 8, 'europe_link' => 108)
+            array('name' => 'Air', 'ram' => 512, 'slots' => 7, 'usa_link' => 9, 'europe_link' => 100, 'price' => '5.00'),
+            array('name' => 'Dirt', 'ram' => 1024, 'slots' => 15, 'usa_link' => 1, 'europe_link' => 101, 'price' => '8.00'),
+            array('name' => 'Grass', 'ram' => 1536, 'slots' => 23, 'usa_link' => 20, 'europe_link' => 109, 'price' => '13.00'),
+            array('name' => 'Cobblestone', 'ram' => 2048, 'slots' => 30, 'usa_link' => 2, 'europe_link' => 102, 'price' => '18.00'),
+            array('name' => 'Stone', 'ram' => 3072, 'slots' => 45, 'usa_link' => 3, 'europe_link' => 103, 'price' => '28.00'),
+            array('name' => 'Coal', 'ram' => 4096, 'slots' => 60, 'usa_link' => 4, 'europe_link' => 104, 'price' => '38.00'),
+            array('name' => 'Redstone', 'ram' => 6144, 'slots' => 120, 'usa_link' => 6, 'europe_link' => 106, 'price' => '58.00'),
+            array('name' => 'Diamond', 'ram' => 8192, 'slots' => 160, 'usa_link' => 8, 'europe_link' => 108, 'price' => '78.00')
         )
     )
 );
