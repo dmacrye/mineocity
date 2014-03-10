fa03d86d7f4769dd8c70dd0071641c4aa2fb6a8e
Daniel MacLaury <dmacrye@gmail.com>
2014-03-10 13:02:08 -0400

    Config & Pricing

--- dist/config.php
+++ dist/config.php
@@ -3,19 +3,21 @@
 return array(
     'home' => array(
         'locations' => array(
-            array('x' => '10px', 'y' => '30px')
+            array('x' => '-79px', 'y' => '-153px'),
+            array('x' => '-280px', 'y' => '-120px'),
+            array('x' => '-395px', 'y' => '-110px'),
         )
     ),
     'minecraft' => array(
         'plans' => array(
             array('name' => 'Air', 'ram' => 512, 'slots' => 7, 'usa_link' => 9, 'europe_link' => 100, 'price' => '5.00'),
-            array('name' => 'Dirt', 'ram' => 1024, 'slots' => 15, 'usa_link' => 1, 'europe_link' => 101, 'price' => '8.00'),
-            array('name' => 'Grass', 'ram' => 1536, 'slots' => 23, 'usa_link' => 20, 'europe_link' => 109, 'price' => '13.00'),
-            array('name' => 'Cobblestone', 'ram' => 2048, 'slots' => 30, 'usa_link' => 2, 'europe_link' => 102, 'price' => '18.00'),
-            array('name' => 'Stone', 'ram' => 3072, 'slots' => 45, 'usa_link' => 3, 'europe_link' => 103, 'price' => '28.00'),
-            array('name' => 'Coal', 'ram' => 4096, 'slots' => 60, 'usa_link' => 4, 'europe_link' => 104, 'price' => '38.00'),
-            array('name' => 'Redstone', 'ram' => 6144, 'slots' => 120, 'usa_link' => 6, 'europe_link' => 106, 'price' => '58.00'),
-            array('name' => 'Diamond', 'ram' => 8192, 'slots' => 160, 'usa_link' => 8, 'europe_link' => 108, 'price' => '78.00')
+            array('name' => 'Dirt', 'ram' => 1024, 'slots' => 15, 'usa_link' => 1, 'europe_link' => 101, 'price' => '10.00'),
+            array('name' => 'Grass', 'ram' => 1536, 'slots' => 23, 'usa_link' => 20, 'europe_link' => 109, 'price' => '15.00'),
+            array('name' => 'Cobblestone', 'ram' => 2048, 'slots' => 30, 'usa_link' => 2, 'europe_link' => 102, 'price' => '20.00'),
+            array('name' => 'Stone', 'ram' => 3072, 'slots' => 45, 'usa_link' => 3, 'europe_link' => 103, 'price' => '30.00'),
+            array('name' => 'Coal', 'ram' => 4096, 'slots' => 60, 'usa_link' => 4, 'europe_link' => 104, 'price' => '40.00'),
+            array('name' => 'Redstone', 'ram' => 6144, 'slots' => 120, 'usa_link' => 6, 'europe_link' => 106, 'price' => '60.00'),
+            array('name' => 'Diamond', 'ram' => 8192, 'slots' => 160, 'usa_link' => 8, 'europe_link' => 108, 'price' => '80.00')
         )
     )
 );
