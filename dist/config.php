30615efe90f99c14f401e71e396a8ac47f2be511
root <root@web1.mineocityhosting.com>
2015-01-08 09:13:24 -0500

    remove default content

--- dist/config.php
+++ dist/config.php
@@ -22,25 +22,22 @@ return array(
   ),
   'budget' => array(
     'plans' => array(
-      array('name' => 'Air', 'ram' => 512, 'slots' => 7, 'link' => 100, 'price' => '5.00'),
-      array('name' => 'Dirt', 'ram' => 1024, 'slots' => 15, 'link' => 101, 'price' => '10.00'),
-      array('name' => 'Grass', 'ram' => 1536, 'slots' => 23,  'link' => 109, 'price' => '15.00'),
-      array('name' => 'Cobblestone', 'ram' => 2048, 'slots' => 30, 'link' => 102, 'price' => '20.00'),
-      array('name' => 'Stone', 'ram' => 3072, 'slots' => 45, 'link' => 103, 'price' => '30.00'),
-      array('name' => 'Coal', 'ram' => 4096, 'slots' => 60, 'link' => 104, 'price' => '40.00'),
-      array('name' => 'Redstone', 'ram' => 6144, 'slots' => 120, 'link' => 106, 'price' => '60.00'),
-      array('name' => 'Diamond', 'ram' => 8192, 'slots' => 160, 'link' => 108, 'price' => '80.00'),
-      array('name' => 'Diamond', 'ram' => 8192, 'slots' => 160, 'link' => 108, 'price' => '80.00'),
-      array('name' => 'Diamond', 'ram' => 8192, 'slots' => 160, 'link' => 108, 'price' => '80.00'),
-      array('name' => 'Diamond', 'ram' => 8192, 'slots' => 160, 'link' => 108, 'price' => '80.00'),
+      array('name' => 'Tier 1', 'ram' => 512, 'slots' => 7, 'link' => 100, 'price' => '2.50'),
+      array('name' => 'Tier 2', 'ram' => 1024, 'slots' => 15, 'link' => 101, 'price' => '5.00'),
+      array('name' => 'Tier 3', 'ram' => 1536, 'slots' => 23,  'link' => 109, 'price' => '7.50'),
+      array('name' => 'Tier 4', 'ram' => 2048, 'slots' => 30, 'link' => 102, 'price' => '10.00'),
+      array('name' => 'Tier 5', 'ram' => 3072, 'slots' => 45, 'link' => 103, 'price' => '15.00'),
+      array('name' => 'Tier 6', 'ram' => 4096, 'slots' => 60, 'link' => 104, 'price' => '20.00'),
+      array('name' => 'Tier 7', 'ram' => 6144, 'slots' => 120, 'link' => 106, 'price' => '30.00'),
+      array('name' => 'Tier 8', 'ram' => 8192, 'slots' => 160, 'link' => 108, 'price' => '40.00'),
     )
   ),
   'source' => array(
     'plans' => array(
-      array('name' => 'Team Fortress 2', 'additional_slots' => '$10', 'slots' => 7, 'link' => 100, 'price' => '5.00'),
-      array('name' => 'Half-Life 2', 'additional_slots' => '$10', 'slots' => 15, 'link' => 101, 'price' => '10.00'),
-      array('name' => 'Other Game', 'additional_slots' => '$20', 'slots' => 23,  'link' => 109, 'price' => '15.00'),
-      array('name' => 'umm..', 'additional_slots' => '$1000', 'slots' => 30, 'link' => 102, 'price' => '20.00'),
+      array('name' => 'Team Fortress 2', 'additional_slots' => '$0.75', 'slots' => 8, 'link' => 117, 'price' => '4.99'),
+      array('name' => 'Counterstrike Source', 'additional_slots' => '$0.75', 'slots' => 8, 'link' => 120, 'price' => '4.99'),
+      array('name' => 'CS GO', 'additional_slots' => '$0.75', 'slots' => 8,  'link' => 118, 'price' => '4.99'),
+      array('name' => 'Garrys Mod', 'additional_slots' => '$0.75', 'slots' => 8, 'link' => 119, 'price' => '4.99'),
     )
   )
 
